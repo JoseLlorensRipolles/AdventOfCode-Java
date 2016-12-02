0efacde8fb7ed0e3e67cbf2dfbee2853ddf0f7b0
Jose Llorens Ripolles <jose.llorens.ripolles@gmail.com>
2016-12-02 10:28:41 +0100

    Implemented solution to part2

--- AoC/src/Day2/NumPadAdv.java
+++ AoC/src/Day2/NumPadAdv.java
@@ -1,5 +1,54 @@
 package Day2;
 
 public class NumPadAdv {
+  char[][] numPad = new char[][]{{'-','-','D','-','-'}
+                                ,{'-','A','B','C','-'},
+                                 {'5','6','7','8','9'},
+                                 {'-','2','3','4','-'},
+                                 {'-','-','1','-','-'}};
 
+  int i, j;
+
+
+  public NumPadAdv(int i, int j) {
+    this.i = i;
+    this.j = j;
+  }
+
+  public void moveUp(){
+    if(i<4){
+      if(numPad[i+1][j]!='-'){
+        i++;
+      }
+    }
+  }
+
+  public void moveDown(){
+    if(i>0){
+      if(numPad[i-1][j]!='-'){
+        i--;
+      }
+    }
+  }
+
+  public void moveRight(){
+    if(j<4){
+      if(numPad[i][j+1]!='-'){
+        j++;
+      }
+    }
+  }
+
+  public void moveLeft(){
+    if(j>0){
+      if(numPad[i][j-1]!='-'){
+        j--;
+      }
+    }
+  }
+
+
+  public char getChar(){
+    return numPad[i][j];
+  }
 }
--- AoC/test/Day2/NumPadAdvTest.java
+++ AoC/test/Day2/NumPadAdvTest.java
@@ -1,4 +1,36 @@
 package Day2;
 
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class NumPadAdvTest {
+  @Test
+  public void shouldCreateNumPad(){
+    NumPadAdv numPad = new NumPadAdv(2,2);
+    assertThat(numPad.getChar(),is('7'));
+  }
+
+  @Test
+  public void shouldMoveCorrectlyInBounds(){
+    NumPadAdv numPadAdv = new NumPadAdv(2,2);
+    numPadAdv.moveDown();
+    assertThat(numPadAdv.getChar(),is('B'));
+    numPadAdv.moveDown();
+    numPadAdv.moveDown();
+    assertThat(numPadAdv.getChar(),is('D'));
+    numPadAdv.moveRight();
+    numPadAdv.moveLeft();
+    assertThat(numPadAdv.getChar(),is('D'));
+    numPadAdv.moveUp();
+    numPadAdv.moveLeft();
+    numPadAdv.moveLeft();
+    assertThat(numPadAdv.getChar(),is('A'));
+    numPadAdv.moveUp();
+    numPadAdv.moveUp();
+    numPadAdv.moveUp();
+    assertThat(numPadAdv.getChar(),is('2'));
+  }
+
 }
