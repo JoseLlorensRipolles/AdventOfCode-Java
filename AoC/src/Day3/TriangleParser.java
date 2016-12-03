b872d051f763e79429e652816e2cff44c3718aca
Jose Llorens Ripolles <jose.llorens.ripolles@gmail.com>
2016-12-03 17:06:36 +0100

    Created structure for TriangleParser

---
+++ AoC/src/Day3/TriangleParser.java
@@ -0,0 +1,6 @@
+package Day3;
+
+public class TriangleParser {
+  public TriangleParser() {
+  }
+}
---
+++ AoC/test/Day3/TriangleParserTest.java
@@ -0,0 +1,11 @@
+package Day3;
+
+import org.junit.Test;
+
+public class TriangleParserTest {
+
+  @Test
+  public void should_create_triangle_parser(){
+    TriangleParser triangleParser = new TriangleParser();
+  }
+}
