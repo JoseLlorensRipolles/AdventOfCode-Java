ca9ddda4f68e8931059166da250928bd82b59c13
Jose Llorens Ripolles <jose.llorens.ripolles@gmail.com>
2016-12-03 17:41:07 +0100

    Implemented Day3 class (Part 1 solved)

--- AoC/src/Day3/Day3.java
+++ AoC/src/Day3/Day3.java
@@ -1,5 +1,23 @@
 package Day3;
 
-public class Day3 {
+import java.util.Scanner;
 
+public class Day3 {
+  public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+    int count = 0;
+    TriangleParser triangleParser = new TriangleParser();
+    Boolean salir = false;
+    while(in.hasNextLine()&&!salir){
+      String line = in.nextLine();
+      if(line.equals("")){
+        salir = true;
+      }else {
+        if (triangleParser.parse(line).isPossible()) {
+          count++;
+        }
+      }
+    }
+    System.out.println("Number of possible triangles: "+count);
+  }
 }
